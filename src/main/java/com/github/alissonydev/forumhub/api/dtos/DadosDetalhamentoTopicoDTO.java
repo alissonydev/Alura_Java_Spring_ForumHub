95b2add26e249a2cb26ba4b34b34caf8c5c12f22
alissonydev <alissonydev@outlook.com>
2026-02-27 12:11:57 -0300

    DadosDetalhamentoTopicoDTO

--- src/main/java/com/github/alissonydev/forumhub/api/dtos/DadosDetalhamentoTopicoDTO.java
+++ src/main/java/com/github/alissonydev/forumhub/api/dtos/DadosDetalhamentoTopicoDTO.java
@@ -1,20 +1,35 @@
 package com.github.alissonydev.forumhub.api.dtos;
 
 import com.github.alissonydev.forumhub.domains.topicos.Topico;
+import com.github.alissonydev.forumhub.domains.topicos.enums.TopicoStatus;
+
+import java.util.List;
 
 public record DadosDetalhamentoTopicoDTO(
 
         Long id ,
         String titulo ,
         String mensagem ,
-        String dataCriacao
+        String dataCriacao ,
+        TopicoStatus status ,
+        String nomeAutor ,
+        String nomeCurso ,
+        List<DadosRespostaDTO> respostas
 ) {
     public DadosDetalhamentoTopicoDTO(Topico topico) {
         this(
                 topico.getId() ,
                 topico.getTitulo() ,
                 topico.getMensagem() ,
-                topico.getDataCriacao().toString()
+                topico.getDataCriacao().toString() ,
+                topico.getStatus() ,
+                topico.getAutor() ,
+                topico.getCurso() ,
+
+                topico.getResposta()
+                        .stream()
+                        .map(DadosRespostaDTO::new)
+                        .toList()
         );
     }
 }
